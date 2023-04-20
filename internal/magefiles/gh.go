5f4e9b8ed3e4bf6075daf9063abd84e3d87e8680
Yufei Li <yufeiminds@gmail.com>
2023-04-20 22:50:43 +0800

    chore: resolve config drifting and add two terraform modules

--- internal/magefiles/gh.go
+++ internal/magefiles/gh.go
@@ -5,6 +5,8 @@ package main
 
 import (
 	"context"
+	"os"
+	"strings"
 
 	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
@@ -15,12 +17,21 @@ type GH mg.Namespace
 
 const pulumiFolder = "internal/iac/github"
 
+var osEnv = make(map[string]string)
+
+func init() {
+	for _, env := range os.Environ() {
+		tokens := strings.SplitN(env, "=", 2)
+		osEnv[tokens[0]] = tokens[1]
+	}
+}
+
 // Apply reconcile GitHub configuration
 func (gh GH) Apply(ctx context.Context) error {
-	return sh.RunV("pulumi", "up", "-y", "-C", pulumiFolder)
+	return sh.RunWithV(osEnv, "pulumi", "up", "-y", "-C", pulumiFolder)
 }
 
 // Review show changes be triggered
 func (gh GH) Review(ctx context.Context) error {
-	return sh.RunV("pulumi", "preview", "-C", pulumiFolder)
+	return sh.RunWithV(osEnv, "pulumi", "preview", "-C", pulumiFolder)
 }
